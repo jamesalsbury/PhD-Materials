e7fefcc06fd7f0603fef8ccc09599ac3e1515854
James Salsbury <jsalsbury1@sheffield.ac.uk>
2021-11-19 16:19:49 +0000

    Adding shiny app online

--- Survival/DTE/Assurance/app.R
+++ Survival/DTE/Assurance/app.R
@@ -86,58 +86,79 @@ server <- function(input, output, session) {
          y = rev(seq(0.001, 0.999, by = 0.001)), type="l", xlab="Time (months)", ylab="Survival",
          col = "blue", xlim=c(0,max(simdata$time)*1.1))
     
+    lambda2 <- exp(-fitcontrol$coefficients)
+    gamma2 <- 1/fitcontrol$scale
     
+    fn <- function(x) {
+      
+      first <- x[1]*x[2]*(x[1]*(input$T1Mean+input$T2Mean))^(x[2]-1) - input$T2HRMean*(lambda2*gamma2*(lambda2*(input$T1Mean+input$T2Mean))^(gamma2-1))
+      second <- x[1]*x[2]*(x[1]*(input$T1Mean+input$T2Mean+input$T3Mean))^(x[2]-1) - (lambda2*gamma2*(lambda2*(input$T1Mean+input$T2Mean+input$T3Mean))^(gamma2-1))
+      
+      return(c(first, second))
+      
+    }
     
+    t1 <- seq(0, input$T1Mean, by=0.01)
+    HR1 <- rep(1, length(t1))
+    y <- nleqslv(c(1,5), fn)
+    lambda1 <- y[[1]][1]
+    gamma1 <- y[[1]][2]
+    t2 <- seq(input$T1Mean, max(simdata$time)*1.1, by=0.01)
+    HR2 <- (lambda1*gamma1*(lambda1*t2)^(gamma1-1))/(lambda2*gamma2*(lambda2*t2)^(gamma2-1))
+    lines(t1, HR1)
+    lines(t2, HR2)
+    t3 <- seq(HR2[1], 1, by=0.01)
+    HR3 <- rep(input$T1Mean, length(t3))
+    lines(HR3, t3)
+    
+    effectt <- seq(0, input$T1Mean, by=0.01)
+    effecty <- exp(-((exp(-fitcontrol$coefficients))*effectt)^(1/fitcontrol$scale))
+    lines(effectt, effecty, col="green", lty=3)
+    
+    aftereffectt <- seq(input$T1Mean, max(simdata$time)*1.1, by=0.01)
+    aftereffecty <- exp(-(exp(-fitcontrol$coefficients)*input$T1Mean)^(1/fitcontrol$scale)-(lambda1^gamma1)*(aftereffectt^gamma1-input$T1Mean^gamma1))
+    lines(aftereffectt, aftereffecty, col="red", lwd=1.5, lty=2) 
     
     #Need to draw samples from the distributions for T1, T2, T2HR and T3
     
-    for (i in 1:10){
-      lambda2 <- exp(-fitcontrol$coefficients)
-      gamma2 <- 1/fitcontrol$scale
-      
-      T1 <- rnorm(1, mean = input$T1Mean, sd = sqrt(input$T1Var))
-      
-      T2 <- rnorm(1, mean = input$T2Mean, sd = sqrt(input$T2Var))
-      
-      estBetaParams <- function(mu, var) {
-        alpha <- ((1 - mu) / var - 1 / mu) * mu ^ 2
-        beta <- alpha * (1 / mu - 1)
-        return(params = list(alpha = alpha, beta = beta))
-      }
-      
-      EstT2HR <- estBetaParams(input$T2HRMean, input$T2HRVar)
-      
-      T2HR <- rbeta(1, EstT2HR$alpha, EstT2HR$beta)
-      
-      T3 <- rnorm(1, mean = input$T3Mean, sd = sqrt(input$T3Var))
-      
-      
-      fn <- function(x) {
-        
-        first <- x[1]*x[2]*(x[1]*(T1+T2))^(x[2]-1) - T2HR*(lambda2*gamma2*(lambda2*(T1+T2))^(gamma2-1))
-        second <- x[1]*x[2]*(x[1]*(T1+T2+T3))^(x[2]-1) - (lambda2*gamma2*(lambda2*(T1+T2+T3))^(gamma2-1))
-        
-        return(c(first, second))
-        
-      }
-      
-      y <- nleqslv(c(1,5), fn)
-      lambda1 <<- y[[1]][1]
-      gamma1 <<- y[[1]][2]
-      
-      effectt <- seq(0, T1, by=0.01)
-      effecty <- exp(-((exp(-fitcontrol$coefficients))*effectt)^(1/fitcontrol$scale))
-      lines(effectt, effecty, col="green", lty=3)
-      
-      aftereffectt <- seq(T1, max(simdata$time)*1.1, by=0.01)
-      aftereffecty <- exp(-(exp(-fitcontrol$coefficients)*T1)^(1/fitcontrol$scale)-(lambda1^gamma1)*(aftereffectt^gamma1-T1^gamma1))
-      lines(aftereffectt, aftereffecty, col="red", lty=2) 
-    }
+    # for (i in 1:10){
+    #   
+    #   
+    #   T1 <- rnorm(1, mean = input$T1Mean, sd = sqrt(input$T1Var))
+    #   
+    #   T2 <- rnorm(1, mean = input$T2Mean, sd = sqrt(input$T2Var))
+    #   
+    #   estBetaParams <- function(mu, var) {
+    #     alpha <- ((1 - mu) / var - 1 / mu) * mu ^ 2
+    #     beta <- alpha * (1 / mu - 1)
+    #     return(params = list(alpha = alpha, beta = beta))
+    #   }
+    #   
+    #   EstT2HR <- estBetaParams(input$T2HRMean, input$T2HRVar)
+    #   
+    #   T2HR <- rbeta(1, EstT2HR$alpha, EstT2HR$beta)
+    #   
+    #   T3 <- rnorm(1, mean = input$T3Mean, sd = sqrt(input$T3Var))
+    #   
+    #   
+    #   y <- nleqslv(c(1,5), fn)
+    #   lambda1 <- y[[1]][1]
+    #   gamma1 <- y[[1]][2]
+    #   
+    #   effectt <- seq(0, T1, by=0.01)
+    #   effecty <- exp(-((exp(-fitcontrol$coefficients))*effectt)^(1/fitcontrol$scale))
+    #   lines(effectt, effecty, col="green", lty=3)
+    #   
+    #   aftereffectt <- seq(T1, max(simdata$time)*1.1, by=0.01)
+    #   aftereffecty <- exp(-(exp(-fitcontrol$coefficients)*T1)^(1/fitcontrol$scale)-(lambda1^gamma1)*(aftereffectt^gamma1-T1^gamma1))
+    #   lines(aftereffectt, aftereffecty, col="red", lty=2) 
+    # }
     
     
-    legend("topright", legend = c("Weibull fit to control data", "Proposed treatment survival curve", "Control + Treatment both Weibull"),
-           col=c("blue", "red", "green"), lty=1:3)
+    legend("bottomleft", legend = c("Weibull fit to control data", "Proposed treatment survival curve", "Control + Treatment both Weibull", "Hazard Ratio"),
+           col=c("blue", "red", "green", "black"), lty=1:3, cex=0.75)
     
+
   })
   
   
