6250057e11ee3676968152346a1e62e168d250dd
James Salsbury <jsalsbury1@sheffield.ac.uk>
2021-11-19 10:43:46 +0000

    Moving files

--- Survival/DTE/ChangingT1T2HRT3App.R
+++ Survival/DTE/ChangingT1T2HRT3App.R
@@ -10,10 +10,10 @@ ui <- fluidPage(
   sidebarLayout(
     
     sidebarPanel(
-      numericInput("T1", label="When does the treatment begin to work (months)?", value = 5),
-      numericInput("T2", label= "When is the maximum hazard ratio (months)?", value = 10),
-      numericInput("T2hazard", label= "What is the hazard ratio here?", value = 0.5),
-      numericInput("HazardOne", label= "When does the hazard equal 1 again (months)?", value = 40)
+      numericInput("T1", label="When does the treatment begin to take effect (months)?", value=5),
+      numericInput("T2", label= "How long after the treatment takes effect is the maximum hazard ratio (months)?", value=5),
+      numericInput("T2HR", label= "What is the hazard ratio here?", value=0.5),
+      numericInput("T3", label= "How long after the maxmimum hazard ratio does the hazard ratio equal one again (months)?", value=20)
     ),
     
     mainPanel(
@@ -36,11 +36,11 @@ server <- function(input, output, session) {
   output$plotData <- renderPlot({
     
     lambda2 <- 0.06
-    gamma2 <- 0.8
+    gamma2 <- 2
     
     simdata <<- data.frame(time = rweibull(1000, gamma2, 1/lambda2), cens = rep(1, 1000))
     fitcontrolKM <- survfit(Surv(time, cens)~1, data = simdata)
-    plot(fitcontrolKM, conf.int = F, xlim=c(0,100), ylab="Survival", xlab="Time (months)", col="blue",
+    plot(fitcontrolKM, conf.int = F, xlim=c(0,max(simdata$time)*1.1), ylab="Survival", xlab="Time (months)", col="blue",
          main = "The historical data for the control")
     legend("topright", legend = "Kaplan-Meier curve to the control data", col="blue", lty=1)
     
@@ -51,9 +51,9 @@ server <- function(input, output, session) {
     
     
     fitcontrol <<- survreg(Surv(time, cens)~1, dist="weibull", data = simdata)
-    plot(x = predict(fitcontrol, type = "quantile", p = seq(0.01, 0.99, by=.01))[1,],
-         y = rev(seq(0.01, 0.99, by = 0.01)), type="l", xlab="Time (months)", ylab="Survival",
-         col = "blue", xlim=c(0,100))
+    plot(x = predict(fitcontrol, type = "quantile", p = seq(0.001, 0.999, by=.001))[1,],
+         y = rev(seq(0.001, 0.999, by = 0.001)), type="l", xlab="Time (months)", ylab="Survival",
+         col = "blue", xlim=c(0,max(simdata$time)*1.1))
     
     
     effectt <- seq(0, input$T1, by=0.01)
@@ -65,30 +65,24 @@ server <- function(input, output, session) {
     
     fn <- function(x) {
 
-      first <- x[1]*x[2]*(x[1]*input$T2)^(x[2]-1) - input$T2hazard*(lambda2*gamma2*(lambda2*input$T2)^(gamma2-1))
-      second <- x[1]*x[2]*(x[1]*input$HazardOne)^(x[2]-1) - (lambda2*gamma2*(lambda2*input$HazardOne)^(gamma2-1))
+      first <- x[1]*x[2]*(x[1]*(input$T1+input$T2))^(x[2]-1) - input$T2HR*(lambda2*gamma2*(lambda2*(input$T1+input$T2))^(gamma2-1))
+      second <- x[1]*x[2]*(x[1]*(input$T1+input$T2+input$T3))^(x[2]-1) - (lambda2*gamma2*(lambda2*(input$T1+input$T2+input$T3))^(gamma2-1))
 
       return(c(first, second))
 
     }
 
     y <- nleqslv(c(1,5), fn)
-    lambda1 <- y[[1]][1]
-    gamma1 <- y[[1]][2]
+    lambda1 <<- y[[1]][1]
+    gamma1 <<- y[[1]][2]
 
-    aftereffectt <- seq(input$T1, 100, by=0.01)
+    aftereffectt <- seq(input$T1, max(simdata$time)*1.1, by=0.01)
     aftereffecty <- exp(-(exp(-fitcontrol$coefficients)*input$T1)^(1/fitcontrol$scale)-(lambda1^gamma1)*(aftereffectt^gamma1-input$T1^gamma1))
     lines(aftereffectt, aftereffecty, col="red", lty=2)
 
     legend("topright", legend = c("Weibull fit to control data", "Proposed treatment survival curve", "Control + Treatment both Weibull"),
            col=c("blue", "red", "green"), lty=1:3)
   
-  })
-  
-  observeEvent(input$reset, {
-    updateNumericInput(session, "cp", value = 5)
-    updateNumericInput(session, inputId = "lambda1", value = signif(as.numeric(exp(-fitcontrol$coefficients)), 2))
-    updateNumericInput(session, inputId  = "gamma1", value = signif(1/fitcontrol$scale, 2))
   })
   
   
@@ -104,7 +98,7 @@ server <- function(input, output, session) {
   })
   
   output$controlparams <- renderUI({
-    withMathJax(paste0("The parameters seen in the plot above are:$$\\lambda_1 = ",input$lambda1,  "  ,\\gamma_1 = ",input$gamma1 , "$$"))
+    withMathJax(paste0("The parameters seen in the plot above are:$$\\lambda_1 = ", signif(lambda1, 2),  "  ,\\gamma_1 = ", signif(gamma1, 2) , "$$"))
   })
   
   output$treatmentparams <- renderUI({
@@ -115,12 +109,6 @@ server <- function(input, output, session) {
     withMathJax(paste0("$$T = ", input$T1, "$$"))
   })
   
-  output$hazard <- renderUI({
-    # lambda2 <- exp(-fitcontrol$coefficients)
-    # gamma2 <- 1/fitcontrol$scale
-    # t <- seq(0, 50, by=0.01)
-    # HR <- (input$lambda1*input$gamma1*(input$lambda1*t)^(input$gamma1-1))/(lambda2*gamma2*(lambda2*t)^(gamma2-1))
-  })
   
 }
 
