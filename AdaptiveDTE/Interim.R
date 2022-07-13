9410978e449ca33c3311fc0d3912b2be4a413dc0
James Salsbury <jsalsbury1@sheffield.ac.uk>
2022-07-13 15:40:23 +0100

    Looking at IA

--- AdaptiveDTE/Interim.R
+++ AdaptiveDTE/Interim.R
@@ -1,172 +1,45 @@
-
-####
-#Looking at stopping for futility in the case we have
-####
-
+#Libraries
+library(plyr)
 library(survival)
-library(tidyverse)
-#Start off with traditional power
 
-
-#We assume the following parameters for our model
+#We have the following parameters for the control
 lambda2 <- 0.08
 gamma2 <- 0.8
-lambda1 <- 0.04
-gamma1 <- 0.8
 
-#Draw the control curve
+#We draw the control group
 controltime <- seq(0, 100, by=0.01)
 controlsurv <- exp(-(lambda2*controltime)^gamma2)
-plot(controltime, controlsurv, type="l", col="blue", ylim=c(0,1))
+plot(controltime, controlsurv, type="l", col="blue", ylim=c(0,1), xlim=c(0, 20))
 
-#We assume that the delay is 6 months
-bigT <- 6
-
-#Draw the delay curve
-delaytime <- seq(0, bigT, by=0.01)
-delaysurv <- exp(-(lambda2*delaytime)^gamma2)
-lines(delaytime, delaysurv, type="l", col="green")
-
-#Draw the treatment curve
-treatmenttime <- seq(bigT, 100 , by=0.01)
-treatmentsurv <- exp(-(lambda2*bigT)^gamma2-lambda1^gamma1*(treatmenttime^gamma1-bigT^gamma1))
-lines(treatmenttime, treatmentsurv, type="l", col="red")
-
-#So we have no uncertainty about our parameters here
-#Can we construct a power curve for this setup?
-#We only want to run the trial for 60 months
-
-trialLength <- 60
-
-powerfunc <- function(n1, n2){
-  powervec <- rep(NA, 250)
-  eventvec <- rep(NA, 250)
-  for (i in 1:length(powervec)){
-    controldata <- data.frame(time = rweibull(n1, gamma2, 1/lambda2))
-    
-    CP <- exp(-(lambda2*bigT)^gamma2)[[1]]
-    u <- runif(n2)
-    
-    suppressWarnings(z <- ifelse(u>CP, (1/lambda2)*exp(1/gamma2*log(-log(u))), exp((1/gamma1)*log(1/(lambda1^gamma1)*(-log(u)-(lambda2*bigT)^gamma2+lambda1^gamma1*bigT*gamma1)))))
-    
-    DataCombined <- data.frame(time = c(controldata$time, z),
-                               group = c(rep("Control", n1), rep("Treatment", n2)))
-    
-    DataCombined$cens <- DataCombined$time < trialLength
-    
-    DataCombined$cens <- DataCombined$cens*1
-    
-    
-    
-    if (sum(DataCombined$cens)==(n1+n2)){
-      
-    } else {
-      DataCombined[DataCombined$cens==0,]$time <- trialLength
-    }
-    
-    
-    eventvec <- sum(DataCombined$cens==1)
-    
-    test <- survdiff(Surv(time, cens)~group, data = DataCombined)
-    powervec[i] <- test$chisq > qchisq(0.95, 1)
-  }
-  return(list(powervec = mean(powervec), eventvec=mean(na.omit(eventvec))))
-}
-
-
-n1vec <- seq(10, 500, by=20)
-n2vec <- seq(10, 500, by=20)
-powervec <- rep(NA, length(n1vec))
-eventvec <- rep(NA, length(n1vec))
-for (j in 1:length(n1vec)){
-  powervec[j] <- powerfunc(n1vec[j], n2vec[j])$powervec
-  eventvec[j] <- powerfunc(n1vec[j], n2vec[j])$eventvec
-}
-
-nvec <- n1vec+n2vec
-powersmooth <- loess(powervec~nvec)
-par(mar = c(10, 10, 10, 10))
-plot(nvec, predict(powersmooth), ylim=c(0,1), ylab = "Power", xlab="Total sample size", type="l")
-
-eventsmooth <- loess(eventvec~nvec)
-
-par(new = TRUE)		
-
-plot(nvec, predict(eventsmooth), ylab = "", xlab="", type="l", axes=F)
-axis(side = 4, at = seq(0, 1000, by=100))
-mtext("Events", side = 4, line = 3)
-
-
-#How would we stop for futility in this scenario?
-#We could take the results so far, calculate the hazard ratio post-treatment and simulate data according to this hazard ratio
-#We need to have already gone past the delay
-#We then calculate conditional power according to these simulated observations
-#If CP lower than 0.1, say, we stop for futility
-#We base on events rather than sample size
-
-#Lets assume we have an interim look at 10 months
-
-#For 80% power, we need 320 patients
-#We expect to see 300 events
-#So if we do it event driven
-
-#We need to simulate clinical trials with the following differences: time of delay and post-delay HR
-#We look at the data say 10 months in
-
-
-#We have planned for a HR of 0.574
-
-#We only consider uncertainty in length of delay now
-
-#Length of delay is 8 months, when is the best time to conduct a futility look?
-#But why are we looking at futility, when the truth is that it does work?
-
-
-#Why don't we look at BPP here, as this is better defined than an arbitrary choice for CP
-
-#Before we do this, we need to calculate sample size by using assurance
-
-#We assume the following parameters for our model
-lambda2 <- 0.08
-gamma2 <- 0.8
+#We have the following parameters for the treatment
 gamma1 <- 0.8
 
-#We have elicited the follow distributions
-#bigT <- norm(6, 0.5)
+#We have elicited the following distributions
+#bigT <- normal(6, 1)
 #HR <- beta(10, 6)
 
-#Draw the control curve
-controltime <- seq(0, 100, by=0.01)
-controlsurv <- exp(-(lambda2*controltime)^gamma2)
-plot(controltime, controlsurv, type="l", col="blue", ylim=c(0,1))
+#Therefore, using the mean values from the above distribtuions, we have the following treatment curve
+bigTMean <- 6
+HRMean <- 10/16
 
-#We assume that the delay is 6 months
-bigT <- 6
-
-#Draw the delay curve
-delaytime <- seq(0, bigT, by=0.01)
-delaysurv <- exp(-(lambda2*delaytime)^gamma2)
-lines(delaytime, delaysurv, type="l", col="green")
-
-#Draw the treatment curve
-#HR mean is 0.625
-HR <- 0.625
-lambda1 <- exp((log(HR)/gamma2)+log(lambda2))
-treatmenttime <- seq(bigT, 100 , by=0.01)
-treatmentsurv <- exp(-(lambda2*bigT)^gamma2-lambda1^gamma1*(treatmenttime^gamma1-bigT^gamma1))
+lambda1 <- exp((log(HRMean)/gamma2)+log(lambda2))
+treatmenttime <- seq(bigTMean, 100 , by=0.01)
+treatmentsurv <- exp(-(lambda2*bigTMean)^gamma2-lambda1^gamma1*(treatmenttime^gamma1-bigTMean^gamma1))
 lines(treatmenttime, treatmentsurv, type="l", col="red")
 
-#So we have no uncertainty about our parameters here
-#Can we construct a power curve for this setup?
-#We only want to run the trial for 60 months
 
+#So this is what we are planning our trial on
+
+#We can use this to calculate the sample size needed for 80% assurance (power)
+
+#We assume the maximum length of trial will be 60 months
 trialLength <- 60
 
 assfunc <- function(n1, n2){
   assvec <- rep(NA, 250)
   eventvec <- rep(NA, 250)
   for (i in 1:length(assvec)){
-    bigT <- rnorm(1, 6, 0.5)
+    bigT <- rnorm(1, 6, 1)
     HR <- rbeta(1, 10, 6)
     lambda1 <- exp((log(HR)/gamma2)+log(lambda2))
     
@@ -218,33 +91,39 @@ plot(nvec, predict(asssmooth), ylim=c(0,1), ylab = "Assurance", xlab="Total samp
 
 eventsmooth <- loess(eventvec~nvec)
 
-par(new = TRUE)		
+#How many patients do we need for 80% assurance?
 
-plot(nvec, predict(eventsmooth), ylab = "", xlab="", type="l", axes=F)
-axis(side = 4, at = seq(0, 1000, by=100))
-mtext("Events", side = 4, line = 3)
 
-predict(asssmooth, newdata = 500)
+for (i in 20:1000){
+  if (predict(asssmooth, newdata = i)>0.8){
+    break
+  }
+}
 
-predict(eventsmooth, newdata = 500)
+npatients <- round_any(i, 2)
 
-#We require 464 events for 80% power, 500 patients
+#How many events do we expect to see with this number of patients?
 
-#Lets simulate some data, we look at 50% through the information fraction, so we look when 232 events have happened
+events <- round(predict(eventsmooth, newdata = npatients))
+
+#We need to determine when is best to perform any IA
+
+#We require 458 events for 80% assurance, 496 patients
+
+#Lets simulate some data, we look at 50% through the information fraction, so we look when 229 events have happened
 #We simulate the delay to be 8 months, but keep the HR the same
 #We can combine the data with the prior to compute BPP
 
-#We simulate 250 patients in each arm
 
-n1 <- 250
-n2 <- 250
+n1 <- npatients/2
+n2 <- npatients/2
 
 lambda2 <- 0.08
 gamma2 <- 0.8
 gamma1 <- 0.8
 HR <- 0.625
 lambda1 <- exp((log(HR)/gamma2)+log(lambda2))
-bigT <- 8
+bigT <- 3
 
 controldata <- data.frame(time = rweibull(n1, gamma2, 1/lambda2))
 CP <- exp(-(lambda2*bigT)^gamma2)[[1]]
@@ -256,9 +135,47 @@ DataCombined <- data.frame(time = c(controldata$time, z),
                            group = c(rep("Control", n1), rep("Treatment", n2)))
 
 
-DataCombined[order(DataCombined$time),][232,]$time
+IATime <- DataCombined[order(DataCombined$time),][events*0.75,]$time
 
-DataCombined$cens <-  DataCombined$time<DataCombined[order(DataCombined$time),][232,]$time
+#We are performing tha IA at 75% through thr IF
+
+#What will the data set look like here?
+
+DataCombined$cens <- DataCombined$time < IATime
+
+OriginalData <- DataCombined
+
+for (i in 1:nrow(DataCombined)){
+  if (DataCombined$time[i]>IATime){
+    DataCombined$time[i]=IATime
+  }
+}
+
+#Need to work out what direction to go down
+if (bigT<bigTMean){
+  
+}
+
+
+
+#How to estimate the HR
+
+#We need to use the control data to esimtate lambda2 and gamma2
+controlSample <- read_excel(chosenFile$datapath, sheet=1)
+weibfit <- survreg(Surv(time, cens)~1, data = controlSample, dist = "weibull")
+updateTextInput(session, "lambda2", value = round(as.numeric(1/(exp(weibfit$icoef[1]))), 3))
+updateTextInput(session, "gamma2", value = round(as.numeric(exp(-weibfit$icoef[2])), 3))
+
+
+
+
+
+
+
+
+
+
+
 
 
 
