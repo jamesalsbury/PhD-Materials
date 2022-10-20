afd2a9d4e33d6ce62b9b2df971d393125a3bb1ad
James Salsbury <jamesalsbury1@gmail.com>
2022-10-20 21:12:34 +0100

    adding mcmc sample to be uploaded for control

--- Test App/App.R
+++ Test App/App.R
@@ -223,14 +223,11 @@ server = function(input, output, session) {
   #Reactive which determines whether addKM button has been clicked
   v <- reactiveValues(upload = NULL)
  
-  
   #Simulates control curves and plots time-wise CI
   drawsimlinescontrol <- reactive({
     
     #Generate 500 control curves
-    nsamples <- length(inputData()$scale)
-    
-    print(nsamples)
+    nsamples <- 500
     
     time <- seq(0, exp((1.527/input$gammacmean)-log(input$lambdacmean))*1.1, by=0.01)
     
@@ -241,30 +238,38 @@ server = function(input, output, session) {
     SimMatrix <- matrix(NA, nrow = nsamples, ncol=length(quicktime))
     
     #Sample lambdac and gammac from the inputs
-     
     
     lambdacsample <- inputData()$scale
     gammacsample <- inputData()$shape
     
-    
     #Fills matrix with control curves
-    for (i in 1:500){
-      chosenMCMCsample <- sample(1:nsamples)
+    for (i in 1:nsamples){
+      chosenMCMCsample <- sample(1:nsamples, size = 1)
+      #print(lambdacsample[chosenMCMCsample])
+      #print(gammacsample[chosenMCMCsample])
       SimMatrix[i,] <- exp(-(lambdacsample[chosenMCMCsample]*quicktime)^gammacsample[chosenMCMCsample])
+      #print(SimMatrix[i,])
     }
+    
+    #SimMatrix[i,] <- exp(-(lambdacsample[i]*quicktime)^gammacsample[i])
     
     #We now look at each time iteration at the distribution
     #We look at the 0.1 and 0.9 quantile of the distribution
     #These quantiles can be thought of as confidence intervals for the control curve
     
+    print(is.na(SimMatrix))
+    
     lowerbound <- rep(NA, length(quicktime))
     upperbound <- rep(NA, length(quicktime))
     mediancontrol <- rep(NA, length(quicktime))
     for (j in 1:length(quicktime)){
-      lowerbound[j] <- quantile(SimMatrix[,j], 0.1)
-      upperbound[j] <- quantile(SimMatrix[,j], 0.9)
+      lowerbound[j] <- quantile(SimMatrix[,j], 0.05)
+      upperbound[j] <- quantile(SimMatrix[,j], 0.95)
       mediancontrol[j] <- quantile(SimMatrix[,j], 0.5)
     }
+    
+    print(SimMatrix[,5])
+    
     
     return(list(lowerbound=lowerbound, upperbound=upperbound, quicktime=quicktime, SimMatrix = SimMatrix, 
                 mediancontrol = mediancontrol))
@@ -272,50 +277,46 @@ server = function(input, output, session) {
   })
   
   
+  observeEvent(input$uploadSample,{
+    v$upload <- "yes"
+  })
+  
   inputData <- reactive({
     #Allows the user to upload a control sample
-    controlfile <- input$uploadSample
     
-    if (is.null(controlfile)){
+    if (is.null(v$upload)){
       return(NULL)
-      print("no")
     } else {
-      print("yes")
-      controlMCMC <- readRDS(controlfile$datapath)
-      #lambdac and gammac are estimated from the uploaded control sample
-      shape <- controlMCMC[,1]
-      scale <- controlMCMC[,2]
-      updateTextInput(session, "lambdacmean", value = signif(mean(scale), 3))
-      updateTextInput(session, "gammacmean", value = signif(mean(shape), 3))
-      v$upload <- "yes"
-      
-      return(list(shape = shape, scale = scale))
+      chosenFile <- input$uploadSample
+      controlMCMC <- readRDS(chosenFile$datapath)
+          #lambdac and gammac are estimated from the uploaded control sample
+          shape <- as.numeric(controlMCMC[,1])
+          scale <- as.numeric(controlMCMC[,2])
+          updateTextInput(session, "lambdacmean", value = signif(mean(scale), 3))
+          updateTextInput(session, "gammacmean", value = signif(mean(shape), 3))
+          return(list(shape = shape, scale = scale))
     }
     
   })
   
-  # output$recommendedParams <- renderUI({
-  #   if (is.null(inputData())){
-  #     
-  #   } else {
-  #     #Tells the user what the best fitting parameters are for their uploaded sample
-  #     str1 <- paste0("For your uploaded sample, the best fitting parameters are:")
-  #     str2 <- paste0("Lambdac = ", round(inputData()$lambdacmean, 3), " with a standard error of ", round(inputData()$lambdacsd, 3))
-  #     str3 <- paste0("Gammac = ", round(inputData()$gammacmean, 3), " with a standard error of ", round(inputData()$gammacsd, 3))
-  #     HTML(paste(str1, str2, str3, sep = '<br/>'))
-  #   }
-  #   
-  # })
-  # 
-  output$plotControl <- renderPlot({
-    
+  output$recommendedParams <- renderUI({
+    if (is.null(inputData())){
 
-    
+    } else {
+      #Tells the user what the best fitting parameters are for their uploaded sample
+      str1 <- paste0("For your uploaded sample, the best fitting parameters are:")
+      str2 <- paste0("Lambdac = ", signif(mean(inputData()$scale), 3))
+      str3 <- paste0("Gammac = ", signif(mean(inputData()$shape), 3))
+      HTML(paste(str1, str2, str3, sep = '<br/>'))
+    }
+
+  })
+
+  output$plotControl <- renderPlot({
+
     time <- seq(0, exp((1.527/input$gammacmean)-log(input$lambdacmean))*1.1, by=0.01)
     controlsurv <- exp(-(input$lambdacmean*time)^input$gammacmean)
     
-    
-        
     # #Plots the median control curve along with the CI
      controldf <- data.frame(controltime = time, controlcurve = controlsurv)
      mybreaks <- plyr::round_any(seq(0, exp((1.527/input$gammacmean)-log(input$lambdacmean))*1.1, length=5), accuracy = 5)
@@ -330,27 +331,23 @@ server = function(input, output, session) {
        
      } else{
      
-       drawsimlinescontrol()$lowerbound
-     
+       controldf <- data.frame(controltime = drawsimlinescontrol()$quicktime, controlcurve = drawsimlinescontrol()$mediancontrol)
+       controllowerbounddf <- data.frame(x = drawsimlinescontrol()$quicktime, y = drawsimlinescontrol()$lowerbound)
+       controlupperbounddf <- data.frame(x = drawsimlinescontrol()$quicktime, y = drawsimlinescontrol()$upperbound)
+       mybreaks <- plyr::round_any(seq(0, exp((1.527/input$gammacmean)-log(input$lambdacmean))*1.1, length=5), accuracy = 5)
+       
+       theme_set(theme_grey(base_size = 12))
 
-      #Adds the km plot if addKM button clicked
-      mybreaks <- plyr::round_any(seq(0, exp((1.527/input$gammacmean)-log(input$lambdacmean))*1.1, length=5), accuracy = 5)
-      controlSample <- data.frame(time = inputData()$controltime, event = inputData()$controlevent)
-      km <- survival::survfit(Surv(time, event)~1, data = controlSample)
-      theme_set(theme_grey(base_size = 12))
-      p1 <- ggsurvplot(km, data = controlSample, conf.int = T, xlim = c(0, mybreaks[length(mybreaks)]), legend = "none", ylab="Survival", ggtheme = theme_grey(base_size = 12))
-      p1 <- p1$plot +  scale_x_continuous(breaks = mybreaks)
-      p1 <- p1 + geom_line(data = controllowerbounddf, aes(x=x, y=y), linetype="dashed") +
-        geom_line(data = controlupperbounddf, aes(x=x, y=y), linetype="dashed") +
-        geom_line(data = controldf, aes(x=controltime, y=controlcurve), colour = "blue")
-      # p1 <- p1 + scale_x_continuous(breaks = mybreaks)
-      print(p1)
+       
+       p1 <- ggplot(data=controldf, aes(x=controltime, y=controlcurve)) + xlim(0, mybreaks[length(mybreaks)]) + 
+         geom_line(colour="blue") + xlab("Time") + ylab("Survival") + ylim(0,1) + geom_line(data = controllowerbounddf, aes(x=x, y=y), linetype="dashed")+
+         geom_line(data = controlupperbounddf, aes(x=x, y=y), linetype="dashed") + scale_x_continuous(breaks = mybreaks)
+       
+       print(p1)
+       
 
     }
-    
-    #legend.title=element_blank()
-    
-    
+
     
   })
   
@@ -673,7 +670,13 @@ server = function(input, output, session) {
   output$plotFeedback <- renderPlot({
     #This plots the feedback plot
     
-    controldf <- data.frame(controltime = drawsimlinescontrol()$quicktime, controlcurve = drawsimlinescontrol()$mediancontrol)
+    if (is.null(v$upload)){
+      time <- seq(0, exp((1.527/input$gammacmean)-log(input$lambdacmean))*1.1, by=0.01)
+      controlsurv <- exp(-(input$lambdacmean*time)^input$gammacmean)
+      controldf <- data.frame(controltime = time, controlcurve = controlsurv)
+    } else {
+      controldf <- data.frame(controltime = drawsimlinescontrol()$quicktime, controlcurve = drawsimlinescontrol()$mediancontrol)
+    }
     theme_set(theme_grey(base_size = 12))
     p1 <- ggplot(data=controldf, aes(x=controltime, y=controlcurve)) +
       geom_line(colour="blue") + xlab("Time") + ylab("Survival") + ylim(0,1)
@@ -823,8 +826,14 @@ server = function(input, output, session) {
           HR <- 1
         }
         
-        lambdac <- rnorm(1, mean = input$lambdacmean, sd = input$lambdacsd)
-        gammac <- rnorm(1, mean = input$gammacmean, sd = input$gammacsd)
+        if (is.null(v$upload)){
+          lambdac <- input$lambdacmean
+          gammac <- input$gammacmean
+        } else {
+          lambdac <- sample(as.numeric(inputData()$scale), size = 1)
+          gammac <- sample(as.numeric(inputData()$shape), size = 1)
+        }
+        
         gammat <- gammac
         
         lambdat <- lambdac*HR^(1/gammac)
