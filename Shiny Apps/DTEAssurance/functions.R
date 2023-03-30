bdfc461948ae9f57d1181c382475e3aef6211ece
James Salsbury <jsalsbury1@sheffield.ac.uk>
2023-03-30 12:26:39 +0100

    updating recruitment

--- Shiny Apps/DTEAssurance/functions.R
+++ Shiny Apps/DTEAssurance/functions.R
@@ -8,22 +8,24 @@ SimDTEDataSet <- function(n1, n2, gamma1, gamma2, lambda1, lambda2, bigT, recTim
   dataCombined <- data.frame(time = c(rweibull(n1, gamma2, 1/lambda2), treatmenttime),
                              group = c(rep("Control", n1), rep("Treatment", n2)))
   
+  #Samples a recruitment time for each patient
+  dataCombined$recTime <- runif(n1+n2, min = 0, max =  recTime)
   
-  #Adds a random uniformly distributed value, based on the recruitment time
-  dataCombined$time <- dataCombined$time + runif(n1+n2, min = 0, max = recTime)
+  #Pseudo-time calculates how far through the trial the patient has the event (from the beginning)
+  dataCombined$psuedoTime <- dataCombined$time + dataCombined$recTime
   
-  #If the time is less than the total trial length time then the event has happened
-  dataCombined$event <- dataCombined$time < censTime
+  #Looks to see whether the patient will be censored (or not)
+  dataCombined$status <- dataCombined$psuedoTime < censTime
   
   #Making it a binary value (rather than T/F), for ease to read
-  dataCombined$event <- dataCombined$event*1
+  dataCombined$status <- dataCombined$status*1
   
-  #Need to set the event time to be the censoring time
-  if (sum(dataCombined$event)==(n1+n2)){
-    
-  } else{
-    dataCombined[dataCombined$time>censTime,]$time <- censTime
-  }
+  #Ensuring only the patients enrolled by the censoring time are included in the data set
+  dataCombined$enrolled <- dataCombined$recTime < censTime
+  dataCombined <-  dataCombined[dataCombined$enrolled==T,]
+  
+  #Calculates the actual survival time for patients in the trial
+  dataCombined$survival_time <- ifelse(dataCombined$psuedoTime>censTime, censTime  - dataCombined$recTime, dataCombined$time)
   
   return(dataCombined)
 }
