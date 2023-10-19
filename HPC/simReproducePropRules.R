2fa1ed23f7aba9cedaff8ff6e1a0bb688e5acd92
James Salsbury <jsalsbury1@sheffield.ac.uk>
2023-10-19 16:43:35 +0100

    comparing different proposed rules

--- HPC/simReproducePropRules.R
+++ HPC/simReproducePropRules.R
@@ -47,37 +47,37 @@ library(doParallel)
 #     recTime = 12
 #   ))
 
-# HR1Vec <- c(0.6, 0.75, 0.9, 1, 1.3)
+# HR1Vec <- c(0.75, 1, 1.3)
 # T1Vec <- c(0, 3, 6, 9)
 # HR2Vec <- c(0.75, 1, 1.3)
 # recTimeVec <- seq(0, 40, by=10)
 
 # 
-HR1Vec <- 1.2
-T1Vec <- 0
-HR2Vec <-  1.2
-recTimeVec <- 10
+# HR1Vec <- 1.2
+# T1Vec <- 0
+# HR2Vec <-  1.2
+# recTimeVec <- 10
 #recTimeVec <- seq(0, 30, by=5)
 
-count <- 1
-
-n <- length(HR1Vec)*length(T1Vec)*length(HR2Vec)*length(recTimeVec)  # Number of elements you want in the list
-ScenarioList <- vector("list", length = n)
-
-for (i in 1:length(HR1Vec)){
-  for (j in 1:length(T1Vec)){
-    for (k in 1:length(HR2Vec)){
-      for (l in 1:length(recTimeVec)){
-        ScenarioList[[count]]$HR1 = HR1Vec[i]
-        ScenarioList[[count]]$T1 = T1Vec[j]
-        ScenarioList[[count]]$HR2 = HR2Vec[k]
-        ScenarioList[[count]]$T2 = 1000
-        ScenarioList[[count]]$recTime = recTimeVec[l]
-        count <- count + 1
-      }
-    }
-  }
-}
+# count <- 1
+# 
+# n <- length(HR1Vec)*length(T1Vec)*length(HR2Vec)*length(recTimeVec)  # Number of elements you want in the list
+# ScenarioList <- vector("list", length = n)
+# 
+# for (i in 1:length(HR1Vec)){
+#   for (j in 1:length(T1Vec)){
+#     for (k in 1:length(HR2Vec)){
+#       for (l in 1:length(recTimeVec)){
+#         ScenarioList[[count]]$HR1 = HR1Vec[i]
+#         ScenarioList[[count]]$T1 = T1Vec[j]
+#         ScenarioList[[count]]$HR2 = HR2Vec[k]
+#         ScenarioList[[count]]$T2 = 1000
+#         ScenarioList[[count]]$recTime = recTimeVec[l]
+#         count <- count + 1
+#       }
+#     }
+#   }
+# }
 
 propTime <- 1:5
 propProp <- c(1/4, 1/3, 1/2, 2/3, 3/4)
@@ -100,7 +100,7 @@ paramsList <- list(
   numPatients = 340,
   lambdac = -log(0.5)/12,
   numEventsRequired = 512,
-  NSims = 1e1
+  NSims = 1e0
 )
 
 # Generate control and treatment data
@@ -321,13 +321,15 @@ for (i in 1:length(ScenarioList)){
   mean_OBFpower <- mean(sapply(results, function(result) result$OBFpower))
   mean_OBFCensTime <- mean(sapply(results, function(result) result$OBFCensTime))
   mean_OBFSS <- mean(sapply(results, function(result) result$OBFSS))
+  
+  resultsList <- vector("list", length = length(propList))
                      
  for (k in 1:length(propList)){
-  assign(paste0("Prop", k, "time"),mean(sapply(results, function(x) x$propList[[k]]$time)))
-  assign(paste0("Prop", k, "prop"),mean(sapply(results, function(x) x$propList[[k]]$prop)))
-  assign(paste0("Prop", k, "power"),mean(sapply(results, function(x) x$propList[[k]]$power)))
-  assign(paste0("Prop", k, "CensTime"),mean(sapply(results, function(x) x$propList[[k]]$CensTime)))
-  assign(paste0("Prop", k, "SS"),mean(sapply(results, function(x) x$propList[[k]]$SS)))
+   resultsList[[k]]$time <- mean(sapply(results, function(x) x$propList[[k]]$time))
+   resultsList[[k]]$prop <- mean(sapply(results, function(x) x$propList[[k]]$prop))
+   resultsList[[k]]$power <- mean(sapply(results, function(x) x$propList[[k]]$power))
+   resultsList[[k]]$CensTime <- mean(sapply(results, function(x) x$propList[[k]]$CensTime))
+   resultsList[[k]]$SS <- mean(sapply(results, function(x) x$propList[[k]]$SS))
  }   
                     
                                     
@@ -337,21 +339,49 @@ for (i in 1:length(ScenarioList)){
   
   
   if (i==1){
+    
     outcomeDF <- data.frame(NoIAPower = mean_NoIApower, NoIADuration = mean_NoIAcensTime, NoIASS = mean_NoIASS,
                             WieandPower = mean_Wpower, WieandDuration = mean_WCensTime, WieandSS = mean_WSS,
-                            OBFPower = mean_OBFpower, OBFDuration = mean_OBFCensTime, OBFSS = mean_OBFSS,
-                            PropPower = mean_Proppower, PropDuration = mean_PropCensTime, PropSS = mean_PropSS)
+                            OBFPower = mean_OBFpower, OBFDuration = mean_OBFCensTime, OBFSS = mean_OBFSS)
     
-    colnames(outcomeDF) <- c("No IA Power", "No IA Duration", "No IA SS", 
+    
+    for (k in 1:length(resultsList)){
+      outcomeDF <- cbind(outcomeDF, resultsList[[k]]$time)
+      outcomeDF <- cbind(outcomeDF, resultsList[[k]]$prop)
+      outcomeDF <- cbind(outcomeDF, resultsList[[k]]$power)
+      outcomeDF <- cbind(outcomeDF, resultsList[[k]]$CensTime)
+      outcomeDF <- cbind(outcomeDF, resultsList[[k]]$SS)
+    }
+    
+                            
+    
+    outcomeDFColNames <- c("No IA Power", "No IA Duration", "No IA SS", 
                              "Wieand Power", "Wieand Duration", "Wieand SS",
-                             "OBF Power", "OBF Duration", "OBF SS",
-                             "Prop Power", "Prop Duration", "Prop SS")
+                             "OBF Power", "OBF Duration", "OBF SS")
+    
+    for (k in 1:length(resultsList)){
+      outcomeDFColNames <- c(outcomeDFColNames, paste0("Prop", k, "time"))
+      outcomeDFColNames <- c(outcomeDFColNames, paste0("Prop", k, "prop"))
+      outcomeDFColNames <- c(outcomeDFColNames, paste0("Prop", k, "power"))
+      outcomeDFColNames <- c(outcomeDFColNames, paste0("Prop", k, "CensTime"))
+      outcomeDFColNames <- c(outcomeDFColNames, paste0("Prop", k, "SS"))
+    }
+    
+    colnames(outcomeDF) <- outcomeDFColNames
+    
   } else {
     
     tempVec <- c(mean_NoIApower, mean_NoIAcensTime, mean_NoIASS,
                  mean_Wpower, mean_WCensTime, mean_WSS,
-                 mean_OBFpower, mean_OBFCensTime, mean_OBFSS,
-                 mean_Proppower, mean_PropCensTime, mean_PropSS)
+                 mean_OBFpower, mean_OBFCensTime, mean_OBFSS)
+    
+    for (k in 1:length(resultsList)){
+      tempVec <- cbind(tempVec, resultsList[[k]]$time)
+      tempVec <- cbind(tempVec, resultsList[[k]]$prop)
+      tempVec <- cbind(tempVec, resultsList[[k]]$power)
+      tempVec <- cbind(tempVec, resultsList[[k]]$CensTime)
+      tempVec <- cbind(tempVec, resultsList[[k]]$SS)
+    }
     
     outcomeDF <- rbind(outcomeDF, tempVec)
   }
@@ -363,18 +393,7 @@ HR1Vec <- rep(NA, length(ScenarioList))
 T1Vec <- rep(NA, length(ScenarioList))
 HR2Vec <- rep(NA, length(ScenarioList))
 recTimeVec <- rep(NA, length(ScenarioList))
-# NoIAPowerRank <- rep(NA, length(ScenarioList))
-# WieandPowerRank <- rep(NA, length(ScenarioList))
-# OBFPowerRank <- rep(NA, length(ScenarioList))
-# PropPowerRank <- rep(NA, length(ScenarioList))
-# NoIADurationRank <- rep(NA, length(ScenarioList))
-# WieandDurationRank <- rep(NA, length(ScenarioList))
-# OBFDurationRank <- rep(NA, length(ScenarioList))
-# PropDurationRank <- rep(NA, length(ScenarioList))
-# NoIASSRank <- rep(NA, length(ScenarioList))
-# WieandSSRank <- rep(NA, length(ScenarioList))
-# OBFSSRank <- rep(NA, length(ScenarioList))
-# PropSSRank <- rep(NA, length(ScenarioList))
+
 
 
 for (i in 1:length(ScenarioList)){
@@ -382,31 +401,11 @@ for (i in 1:length(ScenarioList)){
   T1Vec[i] <- ScenarioList[[i]]$T1
   HR2Vec[i] <- ScenarioList[[i]]$HR2
   recTimeVec[i] <- ScenarioList[[i]]$recTime
-  
-  # PowerRank <- rank(-c(outcomeDF[i,]$`No IA Power`, outcomeDF[i,]$`Wieand Power`, outcomeDF[i,]$`OBF Power`, outcomeDF[i,]$`Prop Power`))
-  # NoIAPowerRank[i] <- PowerRank[1]
-  # WieandPowerRank[i] <- PowerRank[2]
-  # OBFPowerRank[i] <- PowerRank[3]
-  # PropPowerRank[i] <- PowerRank[4]
-  # 
-  # DurationRank <- rank(c(outcomeDF[i,]$`No IA Duration`, outcomeDF[i,]$`Wieand Duration`, outcomeDF[i,]$`OBF Duration`, outcomeDF[i,]$`Prop Duration`))
-  # NoIADurationRank[i] <- DurationRank[1]
-  # WieandDurationRank[i] <- DurationRank[2]
-  # OBFDurationRank[i] <- DurationRank[3]
-  # PropDurationRank[i] <- DurationRank[4]
-  # 
-  # SSRank <- rank(c(outcomeDF[i,]$`No IA SS`, outcomeDF[i,]$`Wieand SS`, outcomeDF[i,]$`OBF SS`, outcomeDF[i,]$`Prop SS`))
-  # NoIASSRank[i] <- SSRank[1]
-  # WieandSSRank[i] <- SSRank[2]
-  # OBFSSRank[i] <- SSRank[3]
-  # PropSSRank[i] <- SSRank[4]
+
   
 }
 
 outcomeDF <- cbind(outcomeDF, HR1Vec, T1Vec, HR2Vec, recTimeVec)
-# outcomeDF <- cbind(outcomeDF, NoIAPowerRank, WieandPowerRank, OBFPowerRank, PropPowerRank)
-# outcomeDF <- cbind(outcomeDF, NoIADurationRank, WieandDurationRank, OBFDurationRank, PropDurationRank)
-# outcomeDF <- cbind(outcomeDF, NoIASSRank, WieandSSRank, OBFSSRank, PropSSRank)
 
 outcomeDF
 
