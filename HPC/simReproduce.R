061538cf5a2b79b0b370024186e393d4ee5385fc
James Salsbury <jsalsbury1@sheffield.ac.uk>
2023-11-01 16:28:11 +0000

    tidying up

--- HPC/simReproduce.R
+++ HPC/simReproduce.R
@@ -152,6 +152,133 @@ censFunc <- function(dataset, numObs) {
   return(list(dataCombined = dataset, censTime = censTime, sampleSize = sampleSize))
 }
 
+NoIAFunc <- function(dataset){
+  NoIAOutcome <- censFunc(dataset, paramsList$numEventsRequired)
+  NoIA <- NoIAOutcome$dataCombined
+  NoIAcensTime <- NoIAOutcome$censTime
+  NoIASS <- NoIAOutcome$sampleSize
+  coxmodel <- coxph(Surv(survival_time, status) ~ group, data = NoIA)
+  deltad <- as.numeric(exp(coef(coxmodel)))
+  NoIALRT <- survdiff(Surv(time, status) ~ group, data = NoIA)
+  NoIApower <- (NoIALRT$chisq > qchisq(0.95, 1) & deltad < 1)
+  return(list(NoIApower = NoIApower, NoIAcensTime = NoIAcensTime, NoIASS = NoIASS))
+}
+
+WieandFunc <- function(dataset){
+  WieandOutcome <- "Continue"
+  W1Outcome <- censFunc(dataset, paramsList$numEventsRequired * 0.5)
+  W1 <- W1Outcome$dataCombined
+  W1censTime <- W1Outcome$censTime
+  W1SS <- W1Outcome$sampleSize
+  coxmodel <- coxph(Surv(survival_time, status) ~ group, data = W1)
+  deltad <- as.numeric(exp(coef(coxmodel)))
+  if (deltad > 1) WieandOutcome <- "Stop1"
+  
+  W2Outcome <- censFunc(dataCombined, paramsList$numEventsRequired * 0.75)
+  W2 <- W2Outcome$dataCombined
+  W2censTime <- W2Outcome$censTime
+  W2SS <- W2Outcome$sampleSize
+  coxmodel <- coxph(Surv(survival_time, status) ~ group, data = W2)
+  deltad <- as.numeric(exp(coef(coxmodel)))
+  if (deltad > 1 & WieandOutcome=="Continue") WieandOutcome <- "Stop2"
+  
+  WFinalOutcome <- censFunc(dataCombined, paramsList$numEventsRequired)
+  WFinal <- WFinalOutcome$dataCombined
+  WFinalcensTime <- WFinalOutcome$censTime
+  WFinalSS <- WFinalOutcome$sampleSize
+  coxmodel <- coxph(Surv(survival_time, status) ~ group, data = WFinal)
+  deltad <- as.numeric(exp(coef(coxmodel)))
+  WLRT <- survdiff(Surv(time, status) ~ group, data = WFinal)
+  Wpower <- (WLRT$chisq > qchisq(0.95, 1) & deltad < 1 & WieandOutcome == "Continue")
+  WCensTime <- ifelse(WieandOutcome == "Stop1", W1censTime, ifelse(WieandOutcome == "Stop2", W2censTime, WFinalcensTime))
+  WSS <- ifelse(WieandOutcome == "Stop1", W1SS, ifelse(WieandOutcome == "Stop2", W2SS, WFinalSS))
+  return(list(Wpower = Wpower, WCensTime = WCensTime, WSS = WSS))
+}
+
+OBFFunc <- function(dataset){
+  OBFOutcome <- "Continue"
+  OBF1Outcome <- censFunc(dataset, ceiling(paramsList$numEventsRequired/3))
+  OBF1 <- OBF1Outcome$dataCombined
+  OBF1censTime <- OBF1Outcome$censTime
+  OBF1SS <- OBF1Outcome$sampleSize
+  coxmodel <- coxph(Surv(survival_time, status) ~ group, data = OBF1)
+  sumcoxmodel <- summary(coxmodel)
+  zscore <- -sumcoxmodel$coefficients[4]
+  if (zscore < 0.011) OBFOutcome <- "Stop1"
+  
+  OBF2Outcome <- censFunc(dataCombined, ceiling(2*paramsList$numEventsRequired/3))
+  OBF2 <- OBF2Outcome$dataCombined
+  OBF2censTime <- OBF2Outcome$censTime
+  OBF2SS <- OBF2Outcome$sampleSize
+  coxmodel <- coxph(Surv(survival_time, status) ~ group, data = OBF2)
+  sumcoxmodel <- summary(coxmodel)
+  zscore <- -sumcoxmodel$coefficients[4]
+  if (zscore < 0.864 & OBFOutcome=="Continue") OBFOutcome <- "Stop2"
+  
+  OBFFinalOutcome <- censFunc(dataCombined, paramsList$numEventsRequired)
+  OBFFinal <- OBFFinalOutcome$dataCombined
+  OBFFinalcensTime <- OBFFinalOutcome$censTime
+  OBFFinalSS <- OBFFinalOutcome$sampleSize
+  coxmodel <- coxph(Surv(survival_time, status) ~ group, data = OBFFinal)
+  deltad <- as.numeric(exp(coef(coxmodel)))
+  OBFLRT <- survdiff(Surv(time, status) ~ group, data = OBFFinal)
+  OBFpower <- (OBFLRT$chisq > qchisq(0.95, 1) & deltad < 1 & OBFOutcome == "Continue")
+  OBFCensTime <- ifelse(OBFOutcome == "Stop1", OBF1censTime, ifelse(OBFOutcome == "Stop2", OBF2censTime, OBFFinalcensTime))
+  OBFSS <- ifelse(OBFOutcome == "Stop1", OBF1SS, ifelse(OBFOutcome == "Stop2", OBF2SS, OBFFinalSS))
+  return(list(OBFpower = OBFpower, OBFCensTime = OBFCensTime, OBFSS = OBFSS))
+}
+
+PropFunc <- function(dataset, monthsDelay, propEvents){
+  
+  sortedPseudoTimes <- sort(dataset$pseudo_time)
+  propVec <- numeric(paramsList$numEventsRequired)
+  cutoff_index <- ceiling(paramsList$numEventsRequired/2)
+  
+  for (k in cutoff_index:paramsList$numEventsRequired){
+    censTime <- sortedPseudoTimes[k]
+    status <- as.integer(dataset$pseudo_time <= censTime)
+    filteredData <- dataset[dataset$recTime <= censTime, ]
+    survival_time <- ifelse(status == 1, filteredData$time, censTime - filteredData$recTime)
+    propVec[k] <- mean(survival_time[status==1]>monthsDelay)
+  }
+  
+  propVec[1:(cutoff_index-1)] <- 0
+  
+  Stop1 <- max(paramsList$numEventsRequired*0.5, which.min(propVec<propEvents))
+  Stop2 <- max(paramsList$numEventsRequired*0.75, which.min(propVec<propEvents))
+  
+  PropOutcome <- "Continue"
+  Prop1Outcome <- censFunc(dataset, Stop1)
+  Prop1 <- Prop1Outcome$dataCombined
+  Prop1censTime <- Prop1Outcome$censTime
+  Prop1SS <- Prop1Outcome$sampleSize
+  coxmodel <- coxph(Surv(survival_time, status) ~ group, data = Prop1)
+  deltad <- as.numeric(exp(coef(coxmodel)))
+  if (deltad > 1) PropOutcome <- "Stop1"
+  
+  Prop2Outcome <- censFunc(dataCombined, Stop2)
+  Prop2 <- Prop2Outcome$dataCombined
+  Prop2censTime <- Prop2Outcome$censTime
+  Prop2SS <- Prop2Outcome$sampleSize
+  coxmodel <- coxph(Surv(survival_time, status) ~ group, data = Prop2)
+  deltad <- as.numeric(exp(coef(coxmodel)))
+  if (deltad > 1 & PropOutcome=="Continue") PropOutcome <- "Stop2"
+  
+  PropFinalOutcome <- censFunc(dataCombined, paramsList$numEventsRequired)
+  PropFinal <- PropFinalOutcome$dataCombined
+  PropFinalcensTime <- PropFinalOutcome$censTime
+  PropFinalSS <- PropFinalOutcome$sampleSize
+  coxmodel <- coxph(Surv(survival_time, status) ~ group, data = PropFinal)
+  deltad <- as.numeric(exp(coef(coxmodel)))
+  PropLRT <- survdiff(Surv(time, status) ~ group, data = PropFinal)
+  Proppower <- (PropLRT$chisq > qchisq(0.95, 1) & deltad < 1 & PropOutcome == "Continue")
+  PropCensTime <- ifelse(PropOutcome == "Stop1", Prop1censTime, ifelse(PropOutcome == "Stop2", Prop2censTime, PropFinalcensTime))
+  PropSS <- ifelse(PropOutcome == "Stop1", Prop1SS, ifelse(PropOutcome == "Stop2", Prop2SS, PropFinalSS))
+  
+  return(list(Proppower = Proppower, PropCensTime = PropCensTime, PropSS = PropSS))
+  
+}
+
 for (i in 1:length(ScenarioList)){
   
   # Set the number of CPU cores you want to use
@@ -167,120 +294,26 @@ for (i in 1:length(ScenarioList)){
                                  ScenarioList[[i]]$T1, ScenarioList[[i]]$HR2, ScenarioList[[i]]$T2, paramsList$numPatients, ScenarioList[[i]]$recTime)
     
     # Do it with no interim analysis first
-    NoIAOutcome <- censFunc(dataCombined, paramsList$numEventsRequired)
-    NoIA <- NoIAOutcome$dataCombined
-    NoIAcensTime <- NoIAOutcome$censTime
-    NoIASS <- NoIAOutcome$sampleSize
-    coxmodel <- coxph(Surv(survival_time, status) ~ group, data = NoIA)
-    deltad <- as.numeric(exp(coef(coxmodel)))
-    NoIALRT <- survdiff(Surv(time, status) ~ group, data = NoIA)
-    NoIApower <- (NoIALRT$chisq > qchisq(0.95, 1) & deltad < 1)
+    NoIAOutcome <- NoIAFunc(dataCombined)
+    NoIApower <-  NoIAOutcome$NoIApower
+    NoIAcensTime <- NoIAOutcome$NoIAcensTime
+    NoIASS <- NoIAOutcome$NoIASS
     
     # Wieand rule
-    WieandOutcome <- "Continue"
-    W1Outcome <- censFunc(dataCombined, paramsList$numEventsRequired * 0.5)
-    W1 <- W1Outcome$dataCombined
-    W1censTime <- W1Outcome$censTime
-    W1SS <- W1Outcome$sampleSize
-    coxmodel <- coxph(Surv(survival_time, status) ~ group, data = W1)
-    deltad <- as.numeric(exp(coef(coxmodel)))
-    if (deltad > 1) WieandOutcome <- "Stop1"
+    WieandOutcome <- WieandFunc(dataCombined)
+    Wpower <-  WieandOutcome$Wpower
+    WCensTime <- WieandOutcome$WCensTime
+    WSS <- WieandOutcome$WSS
     
-    W2Outcome <- censFunc(dataCombined, paramsList$numEventsRequired * 0.75)
-    W2 <- W2Outcome$dataCombined
-    W2censTime <- W2Outcome$censTime
-    W2SS <- W2Outcome$sampleSize
-    coxmodel <- coxph(Surv(survival_time, status) ~ group, data = W2)
-    deltad <- as.numeric(exp(coef(coxmodel)))
-    if (deltad > 1 & WieandOutcome=="Continue") WieandOutcome <- "Stop2"
-    
-    WFinalOutcome <- censFunc(dataCombined, paramsList$numEventsRequired)
-    WFinal <- WFinalOutcome$dataCombined
-    WFinalcensTime <- WFinalOutcome$censTime
-    WFinalSS <- WFinalOutcome$sampleSize
-    coxmodel <- coxph(Surv(survival_time, status) ~ group, data = WFinal)
-    deltad <- as.numeric(exp(coef(coxmodel)))
-    WLRT <- survdiff(Surv(time, status) ~ group, data = WFinal)
-    Wpower <- (WLRT$chisq > qchisq(0.95, 1) & deltad < 1 & WieandOutcome == "Continue")
-    WCensTime <- ifelse(WieandOutcome == "Stop1", W1censTime, ifelse(WieandOutcome == "Stop2", W2censTime, WFinalcensTime))
-    WSS <- ifelse(WieandOutcome == "Stop1", W1SS, ifelse(WieandOutcome == "Stop2", W2SS, WFinalSS))
     
     #OBF rule
-    OBFOutcome <- "Continue"
-    OBF1Outcome <- censFunc(dataCombined, ceiling(paramsList$numEventsRequired/3))
-    OBF1 <- OBF1Outcome$dataCombined
-    OBF1censTime <- OBF1Outcome$censTime
-    OBF1SS <- OBF1Outcome$sampleSize
-    coxmodel <- coxph(Surv(survival_time, status) ~ group, data = OBF1)
-    sumcoxmodel <- summary(coxmodel)
-    zscore <- -sumcoxmodel$coefficients[4]
-    if (zscore < 0.011) OBFOutcome <- "Stop1"
+    OBFOutcome <- OBFFunc(dataCombined)
+    OBFpower <-  OBFOutcome$OBFpower
+    OBFCensTime <- OBFOutcome$OBFCensTime
+    OBFSS <- OBFOutcome$OBFSS
     
-    OBF2Outcome <- censFunc(dataCombined, ceiling(2*paramsList$numEventsRequired/3))
-    OBF2 <- OBF2Outcome$dataCombined
-    OBF2censTime <- OBF2Outcome$censTime
-    OBF2SS <- OBF2Outcome$sampleSize
-    coxmodel <- coxph(Surv(survival_time, status) ~ group, data = OBF2)
-    sumcoxmodel <- summary(coxmodel)
-    zscore <- -sumcoxmodel$coefficients[4]
-    if (zscore < 0.864 & OBFOutcome=="Continue") OBFOutcome <- "Stop2"
     
-    OBFFinalOutcome <- censFunc(dataCombined, paramsList$numEventsRequired)
-    OBFFinal <- OBFFinalOutcome$dataCombined
-    OBFFinalcensTime <- OBFFinalOutcome$censTime
-    OBFFinalSS <- OBFFinalOutcome$sampleSize
-    coxmodel <- coxph(Surv(survival_time, status) ~ group, data = OBFFinal)
-    deltad <- as.numeric(exp(coef(coxmodel)))
-    OBFLRT <- survdiff(Surv(time, status) ~ group, data = OBFFinal)
-    OBFpower <- (OBFLRT$chisq > qchisq(0.95, 1) & deltad < 1 & OBFOutcome == "Continue")
-    OBFCensTime <- ifelse(OBFOutcome == "Stop1", OBF1censTime, ifelse(OBFOutcome == "Stop2", OBF2censTime, OBFFinalcensTime))
-    OBFSS <- ifelse(OBFOutcome == "Stop1", OBF1SS, ifelse(OBFOutcome == "Stop2", OBF2SS, OBFFinalSS))
-    
-    #Proposed rule
-    sortedPseudoTimes <- sort(dataCombined$pseudo_time)
-    propVec <- numeric(paramsList$numEventsRequired)
-    cutoff_index <- ceiling(paramsList$numEventsRequired/2)
-    
-    for (k in cutoff_index:paramsList$numEventsRequired){
-      censTime <- sortedPseudoTimes[k]
-      status <- as.integer(dataCombined$pseudo_time <= censTime)
-      filteredData <- dataCombined[dataCombined$recTime <= censTime, ]
-      survival_time <- ifelse(status == 1, filteredData$time, censTime - filteredData$recTime)
-      propVec[k] <- mean(survival_time[status==1]>3)
-    }
-    
-    propVec[1:(cutoff_index-1)] <- 0
-    
-    Stop1 <- max(paramsList$numEventsRequired*0.5, which.min(propVec<(2/3)))
-    Stop2 <- max(paramsList$numEventsRequired*0.75, which.min(propVec<(2/3)))
-    
-    PropOutcome <- "Continue"
-    Prop1Outcome <- censFunc(dataCombined, Stop1)
-    Prop1 <- Prop1Outcome$dataCombined
-    Prop1censTime <- Prop1Outcome$censTime
-    Prop1SS <- Prop1Outcome$sampleSize
-    coxmodel <- coxph(Surv(survival_time, status) ~ group, data = Prop1)
-    deltad <- as.numeric(exp(coef(coxmodel)))
-    if (deltad > 1) PropOutcome <- "Stop1"
-    
-    Prop2Outcome <- censFunc(dataCombined, Stop2)
-    Prop2 <- Prop2Outcome$dataCombined
-    Prop2censTime <- Prop2Outcome$censTime
-    Prop2SS <- Prop2Outcome$sampleSize
-    coxmodel <- coxph(Surv(survival_time, status) ~ group, data = Prop2)
-    deltad <- as.numeric(exp(coef(coxmodel)))
-    if (deltad > 1 & PropOutcome=="Continue") PropOutcome <- "Stop2"
-    
-    PropFinalOutcome <- censFunc(dataCombined, paramsList$numEventsRequired)
-    PropFinal <- PropFinalOutcome$dataCombined
-    PropFinalcensTime <- PropFinalOutcome$censTime
-    PropFinalSS <- PropFinalOutcome$sampleSize
-    coxmodel <- coxph(Surv(survival_time, status) ~ group, data = PropFinal)
-    deltad <- as.numeric(exp(coef(coxmodel)))
-    PropLRT <- survdiff(Surv(time, status) ~ group, data = PropFinal)
-    Proppower <- (PropLRT$chisq > qchisq(0.95, 1) & deltad < 1 & PropOutcome == "Continue")
-    PropCensTime <- ifelse(PropOutcome == "Stop1", Prop1censTime, ifelse(PropOutcome == "Stop2", Prop2censTime, PropFinalcensTime))
-    PropSS <- ifelse(PropOutcome == "Stop1", Prop1SS, ifelse(PropOutcome == "Stop2", Prop2SS, PropFinalSS))
+    PropOutcome <- PropFunc(dataCombined, 3, 2/3)
     
     
     # Return results
