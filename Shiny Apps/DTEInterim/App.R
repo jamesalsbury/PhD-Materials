749c3cc50a1cd1403b243646566c2ad6344ad247
James Salsbury <jsalsbury1@sheffield.ac.uk>
2024-02-22 17:02:36 +0000

    adding to app

--- Shiny Apps/DTEInterim/App.R
+++ Shiny Apps/DTEInterim/App.R
@@ -26,8 +26,7 @@ ui <- fluidPage(
                    fileInput("samplesFile", "Upload samples")
                  ), 
                  mainPanel = mainPanel(
-                   # Generate plotOutputs dynamically based on the number of delay and HR combinations
-                   tableOutput("futilityTable")
+                   
                  )
                )
       ),
@@ -40,7 +39,21 @@ ui <- fluidPage(
                  ), 
                  mainPanel = mainPanel(
                    # Generate plotOutputs dynamically based on the number of delay and HR combinations
-                   tableOutput("futilityTable")
+                  tableOutput("futilityTable")
+                 )
+               )
+      ),
+      
+      # Two Looks UI ---------------------------------
+      
+      tabPanel("Two Looks", 
+               sidebarLayout(
+                 sidebarPanel = sidebarPanel(
+                   actionButton("calcFutilityTwoLooks", label  = "Calculate", disabled = T)
+                 ), 
+                 mainPanel = mainPanel(
+                   # Generate plotOutputs dynamically based on the number of delay and HR combinations
+                   #tableOutput("futilityTable")
                  )
                )
       )
@@ -53,9 +66,8 @@ ui <- fluidPage(
 # Server logic
 server <- function(input, output, session) {
   
-  # Output Logic ---------------------------------
+  # Data Logic ---------------------------------
   
-
   data <- reactiveValues(treatmentSamplesDF = NULL)
   
   observeEvent(input$samplesFile, {
@@ -63,12 +75,15 @@ server <- function(input, output, session) {
     if (!is.null(file)) {
       # Read the uploaded file into treatmentSamplesDF
       data$treatmentSamplesDF <- read.csv(file$datapath)
-      
+    #print(data$treatmentSamplesDF)
       # Enable the action button when a file is selected
       shinyjs::enable("calcFutilityOneLook")
+      shinyjs::enable("calcFutilityTwoLooks")
     }
   })
   
+  
+  # One Look Logic ---------------------------------
   
   observeEvent(input$calcFutilityOneLook, {
     NRep <- 50
@@ -134,6 +149,100 @@ server <- function(input, output, session) {
       futilityDF
     }, digits = 3)
   })
+  
+  
+  # Two Looks Logic ---------------------------------
+  
+  
+  observeEvent(input$calcFutilityTwoLooks, {
+    NRep <- 1
+    Look1Vec <- seq(0.2, 0.8, by = 0.1)
+    Look2Vec <- seq(0.3, 0.9, by = 0.1)
+    futilityVec <- seq(0.2, 0.9, by = 0.1)
+    
+    
+    # # Create empty data frames
+    # assDF <- SSDF <- DurationDF <- IATimeDF <- data.frame(matrix(NA, nrow = NRep, ncol = length(futilityVec)))
+    # stopDF <- data.frame(matrix(0, nrow = NRep, ncol = length(futilityVec)))
+    # colnames(assDF) <- colnames(SSDF) <- colnames(DurationDF) <- paste0(futilityVec)
+    
+    
+    
+    #withProgress(message = 'Calculating', value = 0, {
+      for (i in 1:NRep){
+        
+        
+        futilityDF <- data.frame(futility = c(futilityVec,1),
+                                 outcome = numeric(length(futilityVec)+1),
+                                 SS = numeric(length(futilityVec)+1),
+                                 duration = numeric(length(futilityVec)+1))
+        
+        treatmentSamplesDF <- data$treatmentSamplesDF
+        
+        #Compute treatment times
+        HRStar <- sample(treatmentSamplesDF[,2], 1)
+        bigT <- sample(treatmentSamplesDF[,1], 1)
+        
+        #Simulate control and treatment data
+        dataCombined <- SimDTEDataSet(input$numPatients, input$lambdac, bigT, HRStar, input$recTime)  
+        
+        #Perform futility look at different Information Fractions
+        finalDF <- CensFunc(dataCombined, input$numEvents)
+        test <- survdiff(Surv(survival_time, status)~group, data = finalDF$dataCombined)
+        coxmodel <- coxph(Surv(survival_time, status)~group, data = finalDF$dataCombined)
+        deltad <- as.numeric(exp(coef(coxmodel)))
+        futilityDF[nrow(futilityDF),2] <- (test$chisq > qchisq(0.95, 1) & deltad<1)
+        futilityDF[nrow(futilityDF),3] <- finalDF$SS
+        futilityDF[nrow(futilityDF),4] <- finalDF$censTime
+        
+        #print(futilityDF)
+        
+        for (k in 1:length(futilityVec)){
+          futilityCens <- CensFunc(dataCombined, input$numEvents*futilityVec[k])
+          futilityLook <- interimLookFunc(futilityCens$dataCombined)
+          futilityDF[k,3] <- futilityCens$SS
+          futilityDF[k,4] <- futilityCens$censTime
+          if (futilityLook!="Stop"){ futilityDF[k,2] <- 1} 
+        }
+        print(futilityDF)
+        
+        assDF <- SSDF <- DurationDF <- data.frame(matrix(NA, nrow = length(Look2Vec), ncol = length(Look1Vec)))
+        
+        for (j in 1:length(Look1Vec)){
+          for (k in j:length(Look2Vec)){
+            assDF[k,j] <- "yes"
+          }
+        }
+        
+        print(assDF)
+        
+        #incProgress(1/NRep)
+      }
+   # })
+    
+    pHat <- colMeans(assDF)
+    
+    LB <- pHat - 1.96 * sqrt(pHat * (1 - pHat) / NRep)
+    
+    UB <- pHat + 1.96 * sqrt(pHat * (1 - pHat) / NRep)
+    
+    
+    futilityDF <- data.frame(IF = futilityVec,
+                             censTime = colMeans(IATimeDF),
+                             ass = paste0(round(pHat, 3), " [", round(LB, 3), ",", round(UB, 3), "]"),
+                             stop = colMeans(stopDF),
+                             SS = colMeans(SSDF),
+                             Duration = colMeans(DurationDF))
+    
+    colnames(futilityDF) <- c("Information Fraction", "IA Time", "Assurance", "% stop","Sample Size", "Duration")
+    
+    output$futilityTable <- renderTable({
+      futilityDF
+    }, digits = 3)
+  })
+  
+  
+  
 }
 
 # Run the Shiny app
