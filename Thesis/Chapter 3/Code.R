110e6df0b48b19bb8a9387eedf708f01289e6bbe
James Salsbury <jsalsbury1@sheffield.ac.uk>
2025-06-23 16:07:05 +0100

    Thesis

--- Thesis/Chapter 3/Code.R
+++ Thesis/Chapter 3/Code.R
@@ -208,10 +208,10 @@ DTEDataSetsFunc <- function(author){
                              status = c(controldata$Status, treatmentdata$Status), 
                              group = c(rep("Control", nrow(controldata)), rep("Treatment", nrow(treatmentdata))))
   
-  kmfit <- survfit(Surv(time, status)~group, data = combinedData)
-  plot(kmfit, conf.int = F, col=c("blue", "red"), xlab = "Time (months)", ylab = ylabel, yaxt = "n",
-       cex.axis=1.5, cex.lab=1.5, cex.main=2)
-  axis(2, at=seq(0, 1, by=0.2), labels=seq(0, 100, by=20))
+  # kmfit <- survfit(Surv(time, status)~group, data = combinedData)
+  # plot(kmfit, conf.int = F, col=c("blue", "red"), xlab = "Time (months)", ylab = ylabel, yaxt = "n",
+  #      cex.axis=1.5, cex.lab=1.5, cex.main=2)
+  # axis(2, at=seq(0, 1, by=0.2), labels=seq(0, 100, by=20))
   
   
   #Finding the Weibull parameters and plotting the line
@@ -232,44 +232,44 @@ DTEDataSetsFunc <- function(author){
   
   treatmenttime1 <- seq(THat, trialLength, by=0.01)
   
+  #Scenario 1
+
+  optimfunc1 <- function(par){
+    diff <- 0
+    gammat <- gammac
+    for (i in 1:length(treatmenttime1)){
+      y <-  kmfit$surv[sum(kmfit$time<treatmenttime1[i])]
+      treatmentsurv <- exp(-(lambdac*THat)^gammac - par[1]^gammat*(treatmenttime1[i]^gammat-THat^gammat))
+      diff <- diff + (y-treatmentsurv)^2
+    }
+    return(diff)
+  }
+
+  s1gammat <- gammac
+  s1lambdat <- optimize(optimfunc1, c(0, 2))$minimum
+  treatmentsurv1 <- exp(-(lambdac*THat)^gammac - s1lambdat^s1gammat*(treatmenttime1^s1gammat-THat^s1gammat))
+ # lines(treatmenttime1, treatmentsurv1, col="red", lty=1)
+
+
+  #Scenario 2
+
+
+  optimfunc2 <- function(par){
+    diff <- 0
+    for (i in 1:length(treatmenttime1)){
+      y <-  kmfit$surv[sum(kmfit$time<treatmenttime1[i])]
+      treatmentsurv <- exp(-(lambdac*THat)^gammac - par[1]^par[2]*(treatmenttime1[i]^par[2]-THat^par[2]))
+      diff <- diff + (y-treatmentsurv)^2
+    }
+    return(diff)
+  }
+
+  optimoutput <-  optim(par = c(0.2, 1), fn = optimfunc2)
+  s2lambdat <- optimoutput$par[1]
+  s2gammat <- optimoutput$par[2]
+  treatmentsurv1 <- exp(-(lambdac*THat)^gammac - s2lambdat^s2gammat*(treatmenttime1^s2gammat-THat^s2gammat))
+  #lines(treatmenttime1, treatmentsurv1, col="red", lty=2)
   
-  # #Scenario 1
-  # 
-  # optimfunc1 <- function(par){
-  #   diff <- 0
-  #   gammat <- gammac
-  #   for (i in 1:length(treatmenttime1)){
-  #     y <-  kmfit$surv[sum(kmfit$time<treatmenttime1[i])]
-  #     treatmentsurv <- exp(-(lambdac*THat)^gammac - par[1]^gammat*(treatmenttime1[i]^gammat-THat^gammat))
-  #     diff <- diff + (y-treatmentsurv)^2 
-  #   }
-  #   return(diff) 
-  # }
-  # 
-  # s1gammat <- gammac
-  # s1lambdat <- optimize(optimfunc1, c(0, 2))$minimum
-  # treatmentsurv1 <- exp(-(lambdac*THat)^gammac - s1lambdat^s1gammat*(treatmenttime1^s1gammat-THat^s1gammat))
-  # lines(treatmenttime1, treatmentsurv1, col="red", lty=1)
-  # 
-  # 
-  # #Scenario 2
-  # 
-  # 
-  # optimfunc2 <- function(par){
-  #   diff <- 0
-  #   for (i in 1:length(treatmenttime1)){
-  #     y <-  kmfit$surv[sum(kmfit$time<treatmenttime1[i])]
-  #     treatmentsurv <- exp(-(lambdac*THat)^gammac - par[1]^par[2]*(treatmenttime1[i]^par[2]-THat^par[2]))
-  #     diff <- diff + (y-treatmentsurv)^2 
-  #   }
-  #   return(diff) 
-  # }
-  # 
-  # optimoutput <-  optim(par = c(0.2, 1), fn = optimfunc2)
-  # s2lambdat <- optimoutput$par[1]
-  # s2gammat <- optimoutput$par[2]
-  # treatmentsurv1 <- exp(-(lambdac*THat)^gammac - s2lambdat^s2gammat*(treatmenttime1^s2gammat-THat^s2gammat))
-  # lines(treatmenttime1, treatmentsurv1, col="red", lty=2)
   
   #Plotting horizontal lines to indicate the delay
   # y <- seq(0, 1, by=0.01)
@@ -280,56 +280,259 @@ DTEDataSetsFunc <- function(author){
   # legend("topright", legend = c("Control", "Method A", "Method B"), col=c("blue", "red", "red"), lty=c(1, 1,2),
   #        cex = 1.5)
   
-  legend("topright", legend = c("Control", "Treatment"), col=c("blue", "red"), lty=c(1, 1),
-         cex = 1.5)
+  # legend("topright", legend = c("Control", "Treatment"), col=c("blue", "red"), lty=c(1, 1),
+  #        cex = 1.5)
   
   #Now look at calculating power under the two different scenarios
   
-  # powerfunc <- function(n, lambdat, gammat){
-  #   powervec <- rep(NA, 1000)
-  #   for (i in 1:length(powervec)){
-  #     #Simulating control times
-  #     u <- runif(n)
-  #     controltimes <- (1/lambdac)*(-log(u))^(1/gammac)
-  # 
-  #     #Simulating treatment times
-  #     u <- runif(n)
-  #     CP <- exp(-(lambdac*THat)^gammac)
-  #     treatmenttimes <- ifelse(u>=CP, (1/lambdac)*(-log(u))^(1/gammac), (1/(lambdat^gammat)*((lambdat*THat)^gammat-log(u)-(lambdac*THat)^gammac))^(1/gammat))
-  # 
-  #     #combining control and treatment
-  #     combinedDataPower <- data.frame(time = c(controltimes, treatmenttimes),
-  #                                     status = rep(1, 2*n), group = c(rep("Control", n), rep("Treatment", n)))
-  # 
-  #     test <- survdiff(Surv(time, status)~group, data = combinedDataPower)
-  #     
-  #     
-  #     #If the p-value of the test is less than 0.05 then assvec = 1, 0 otherwise
-  #     powervec[i] <- test$chisq > qchisq(0.95, 1)
-  #   }
-  # 
-  #   return(mean(powervec))
-  # }
+  powerfunc <- function(n, lambdat, gammat){
+    powervec <- rep(NA, 1000)
+    for (i in 1:length(powervec)){
+      #Simulating control times
+      u <- runif(n)
+      controltimes <- (1/lambdac)*(-log(u))^(1/gammac)
+
+      #Simulating treatment times
+      u <- runif(n)
+      CP <- exp(-(lambdac*THat)^gammac)
+      treatmenttimes <- ifelse(u>=CP, (1/lambdac)*(-log(u))^(1/gammac), (1/(lambdat^gammat)*((lambdat*THat)^gammat-log(u)-(lambdac*THat)^gammac))^(1/gammat))
+
+      #combining control and treatment
+      combinedDataPower <- data.frame(time = c(controltimes, treatmenttimes),
+                                      status = rep(1, 2*n), group = c(rep("Control", n), rep("Treatment", n)))
+
+      test <- survdiff(Surv(time, status)~group, data = combinedDataPower)
+
+
+      #If the p-value of the test is less than 0.05 then assvec = 1, 0 otherwise
+      powervec[i] <- test$chisq > qchisq(0.95, 1)
+    }
+
+    return(mean(powervec))
+  }
   
   
-  # nvec <- round(seq(10, 500, length = 30))
-  # powervecs1 <- mapply(powerfunc, nvec, s1lambdat, s1gammat)
-  # powervecs2 <- mapply(powerfunc, nvec, s2lambdat, s2gammat)
-  # 
-  # powers1smooth <- loess(powervecs1~nvec)
-  #  powers2smooth <- loess(powervecs2~nvec)
-  # 
-  #  plot(nvec*2, predict(powers1smooth), type="l", col="red", ylim=c(0, 1), xlab = "Total sample size", ylab = "Power")
-  #  lines(nvec*2, predict(powers2smooth), col="blue", lty=2)
-  # 
-  # legend("bottomright", legend = c("Method A", "Method B"), col = c("red", "blue"), lty=1:2)
+  nvec <- round(seq(10, 500, length = 30))
+  powervecs1 <- mapply(powerfunc, nvec, s1lambdat, s1gammat)
+  powervecs2 <- mapply(powerfunc, nvec, s2lambdat, s2gammat)
+
+  powers1smooth <- loess(powervecs1~nvec)
+   powers2smooth <- loess(powervecs2~nvec)
+
+   plot(nvec*2, predict(powers1smooth), type="l", col="red", ylim=c(0, 1), xlab = "Total sample size", ylab = "Power",
+        cex.axis=1.5, cex.lab=1.5, cex.main=2)
+   lines(nvec*2, predict(powers2smooth), col="blue", lty=2)
+
+  legend("bottomright", legend = c("Method A", "Method B"), col = c("red", "blue"), lty=1:2, cex = 1.5)
 }
 
-png("KM_All_3.png", units="in", width=15, height=5, res=700)
+png("KM_Power.png", units="in", width=15, height=5, res=700)
 par(mfrow=c(1,3))
 DTEDataSetsFunc("Brahmer")
 DTEDataSetsFunc("Yen")
 DTEDataSetsFunc("Borghaei")
+dev.off()
+
+
+##################
+#Doing Flex_Seq
+##################
+gammac <- 0.8
+gammat <- 0.8
+lambdac <- 0.08
+trialtime <- seq(0, 100, by=0.01)
+controlsurv <- exp(-(lambdac*trialtime)^gammac)
+M <- 500
+SimMatrix <- matrix(NA, nrow = M, ncol = length(trialtime))
+for (i in 1:M){
+  bigT <- truncnorm::rtruncnorm(1, mean = 6, sd = 2.97, a = 0)
+  HR <- rbeta(1, 10.8, 6.87)
+  lambdat <- lambdac*HR^(1/gammac)
+  SimMatrix[i,] <- ifelse(trialtime<=bigT, exp(-(lambdac*trialtime)^gammac), exp(-(lambdac*bigT)^gammac-lambdat^gammat*(trialtime^gammat-bigT^gammat)))
+}
+bigT <- truncnorm::rtruncnorm(1, mean = 6, sd = 2.97, a = 0)
+s1 <- sample(SimMatrix[,round(length(trialtime)*0.25)], 1)
+s2 <- 1
+while (s2>s1){ s2 <- sample(SimMatrix[,round(length(trialtime)*0.6)], 1)}
+estWeib <- function(par){
+  t1 <- exp(-(lambdac*bigT)^gammac-par[1]^par[2]*(25^par[2]-bigT^par[2])) - s1
+  t2 <- exp(-(lambdac*bigT)^gammac-par[1]^par[2]*(60^par[2]-bigT^par[2])) - s2
+  terror <- t1^2+t2^2
+  return(terror)
+}
+
+output <- optim(par = c(0, 1), fn = estWeib)
+lambdat <- output$par[1]
+gammat <- output$par[2]
+treatmentsurv <- ifelse(trialtime<=bigT, exp(-(lambdac*trialtime)^gammac), exp(-(lambdac*bigT)^gammac-lambdat^gammat*(trialtime^gammat-bigT^gammat)))
+
+png("Flexible_Assurance_Process.png", units="in", width=14, height=12, res=700)
+par(mfrow=c(2,2))
+#First plot
+plot(trialtime, controlsurv, type="l", col="blue", xlab = "Time (months)", yaxt = "n", ylab = "Overall survival (%)",
+     cex.axis=1.5, cex.lab=1.5, cex.main=2)
+axis(2, at=seq(0, 1, by=0.1), labels=seq(0, 100, by=10))
+points(bigT, exp(-(lambdac*bigT)^gammac), col = "red", bg = "red", pch = 19)
+legend("topright", legend = c("Control Curve", "Sampled Point"), lty = c(1, NA), col=c("blue", "red"), pch = c(NA, 19))
+#Second plot
+plot(trialtime, controlsurv, type="l", col="blue", xlab = "Time (months)", yaxt = "n", ylab = "Overall survival (%)",
+     cex.axis=1.5, cex.lab=1.5, cex.main=2)
+axis(2, at=seq(0, 1, by=0.1), labels=seq(0, 100, by=10))
+points(bigT, exp(-(lambdac*bigT)^gammac), col = "red", bg = "red", pch = 19)
+points(25, s1, col = "red", bg = "red", pch = 19)
+legend("topright", legend = c("Control Curve", "Sampled Points"), lty = c(1, NA), col=c("blue", "red"), pch = c(NA, 19))
+#Third plot
+plot(trialtime, controlsurv, type="l", col="blue", xlab = "Time (months)", yaxt = "n", ylab = "Overall survival (%)",
+     cex.axis=1.5, cex.lab=1.5, cex.main=2)
+axis(2, at=seq(0, 1, by=0.1), labels=seq(0, 100, by=10))
+points(bigT, exp(-(lambdac*bigT)^gammac), col = "red", bg = "red", pch = 19)
+points(25, s1, col = "red", bg = "red", pch = 19)
+points(60, s2, col = "red", bg = "red", pch = 19)
+legend("topright", legend = c("Control Curve", "Sampled Points"), lty = c(1, NA), col=c("blue", "red"), pch = c(NA, 19))
+#Fourth plot
+plot(trialtime, controlsurv, type="l", col="blue", xlab = "Time (months)", yaxt = "n", ylab = "Overall survival (%)",
+     cex.axis=1.5, cex.lab=1.5, cex.main=2)
+axis(2, at=seq(0, 1, by=0.1), labels=seq(0, 100, by=10))
+points(bigT, exp(-(lambdac*bigT)^gammac), col = "red", bg = "red", pch = 19)
+points(25, s1, col = "red", bg = "red", pch = 19)
+points(60, s2, col = "red", bg = "red", pch = 19)
+lines(trialtime, treatmentsurv, col="red")
+legend("topright", legend = c("Control Curve", "Sampled Points", "Treatment Curve"),
+       lty = c(1, NA, 1), col=c("blue", "red", "red"), pch = c(NA, 19, NA))
+dev.off()
+
+
+
+#############
+#Flexible survival curves
+#############
+
+
+
+png("Flexible_Surv_Curves.png", units="in", width=14, height=12, res=700)
+par(mfrow=c(2,2))
+gammac <- 0.8
+gammat <- 0.8
+lambdac <- 0.08
+trialtime <- seq(0, 100, by=0.01)
+controlsurv <- exp(-(lambdac*trialtime)^gammac)
+plot(trialtime, controlsurv, type="l", col="blue", xlab = "Time (months)",
+     yaxt = "n", ylab = "Overall survival (%)", cex.axis=1.5, cex.lab=1.5, cex.main=2)
+axis(2, at=seq(0, 1, by=0.1), labels=seq(0, 100, by=10))
+for (j in 1:10){
+  bigT <- truncnorm::rtruncnorm(1, mean = 6, sd = 2.97, a = 0)
+  HR <- rbeta(1, 10.8, 6.87)
+  lambdat <- lambdac*HR^(1/gammac)
+  treatmenttime <- ifelse(trialtime<=bigT, exp(-(lambdac*trialtime)^gammac), exp(-(lambdac*bigT)^gammac-lambdat^gammat*(trialtime^gammat-bigT^gammat)))
+  lines(trialtime, treatmenttime, col="red")
+}
+
+legend("topright", legend = c("Control", "Treatment"), lty = 1, col=c("blue", "red"),
+       cex = 1.5)
+
+gammac <- 0.8
+gammat <- 0.8
+lambdac <- 0.08
+trialtime <- seq(0, 100, by=0.01)
+controlsurv <- exp(-(lambdac*trialtime)^gammac)
+plot(trialtime, controlsurv, type="l", col="blue", xlab = "Time (months)",
+     yaxt = "n", ylab = "Overall survival (%)", cex.axis=1.5, cex.lab=1.5, cex.main=2)
+axis(2, at=seq(0, 1, by=0.1), labels=seq(0, 100, by=10))
+M <- 500
+SimMatrix <- matrix(NA, nrow = M, ncol = length(trialtime))
+for (i in 1:M){
+  bigT <- truncnorm::rtruncnorm(1, mean = 6, sd = 2.97, a = 0)
+  HR <- rbeta(1, 10.8, 6.87)
+  lambdat <- lambdac*HR^(1/gammac)
+  SimMatrix[i,] <- ifelse(trialtime<=bigT, exp(-(lambdac*trialtime)^gammac), exp(-(lambdac*bigT)^gammac-lambdat^gammat*(trialtime^gammat-bigT^gammat)))
+}
+lowerbound <- rep(NA, length(trialtime))
+medianbound <- rep(NA, length(trialtime))
+upperbound <- rep(NA, length(trialtime))
+for (j in 1:length(trialtime)){
+  lowerbound[j] <- quantile(SimMatrix[,j], 0.1)
+  medianbound[j] <- quantile(SimMatrix[,j], 0.5)
+  upperbound[j] <- quantile(SimMatrix[,j], 0.9)
+}
+lines(trialtime, medianbound, col="red")
+lines(trialtime, lowerbound, lty=2)
+lines(trialtime, upperbound, lty=2)
+legend("topright", legend = c("Control", "Treatment", "Treatment CI"),
+       lty = c(1, 1, 2), col=c("blue", "red", "black"), cex = 1.5)
+
+gammac <- 0.8
+gammat <- 0.8
+lambdac <- 0.08
+trialtime <- seq(0, 100, by=0.01)
+controlsurv <- exp(-(lambdac*trialtime)^gammac)
+M <- 500
+plot(trialtime, controlsurv, type="l", col="blue", xlab = "Time (months)", yaxt = "n",
+     ylab = "Overall survival (%)", cex.axis=1.5, cex.lab=1.5, cex.main=2)
+axis(2, at=seq(0, 1, by=0.1), labels=seq(0, 100, by=10))
+SimMatrix <- matrix(NA, nrow = M, ncol = length(trialtime))
+for (i in 1:M){
+  bigT <- truncnorm::rtruncnorm(1, mean = 6, sd = 2.97, a = 0)
+  HR <- rbeta(1, 10.8, 6.87)
+  lambdat <- lambdac*HR^(1/gammac)
+  SimMatrix[i,] <- ifelse(trialtime<=bigT, exp(-(lambdac*trialtime)^gammac), exp(-(lambdac*bigT)^gammac-lambdat^gammat*(trialtime^gammat-bigT^gammat)))
+}
+for (j in 1:10){
+  bigT <- truncnorm::rtruncnorm(1, mean = 6, sd = 2.97, a = 0)
+  s1 <- sample(SimMatrix[,round(length(trialtime)*0.25)], 1)
+  s2 <- 1
+  while (s2>s1){ s2 <- sample(SimMatrix[,round(length(trialtime)*0.6)], 1)}
+  estWeib <- function(par){
+    t1 <- exp(-(lambdac*bigT)^gammac-par[1]^par[2]*(25^par[2]-bigT^par[2])) - s1
+    t2 <- exp(-(lambdac*bigT)^gammac-par[1]^par[2]*(60^par[2]-bigT^par[2])) - s2
+    terror <- t1^2+t2^2
+    return(terror)
+  }
+  
+  output <- optim(par = c(0, 1), fn = estWeib)
+  lambdat <- output$par[1]
+  gammat <- output$par[2]
+  treatmentsurv <- ifelse(trialtime<=bigT, exp(-(lambdac*trialtime)^gammac), exp(-(lambdac*bigT)^gammac-lambdat^gammat*(trialtime^gammat-bigT^gammat)))
+  lines(trialtime, treatmentsurv, col="red")
+}
+legend("topright", legend = c("Control", "Treatment"),
+       lty = 1, col=c("blue", "red"), cex = 1.5)
+
+FlexSimMatrix <- matrix(NA, nrow = M, ncol = length(trialtime))
+for (i in 1:M){
+  bigT <- truncnorm::rtruncnorm(1, mean = 6, sd = 2.97, a = 0)
+  s1 <- sample(SimMatrix[,round(length(trialtime)*0.25)], 1)
+  s2 <- 1
+  while (s2>s1){ s2 <- sample(SimMatrix[,round(length(trialtime)*0.6)], 1)}
+  estWeib <- function(par){
+    t1 <- exp(-(lambdac*bigT)^gammac-par[1]^par[2]*(25^par[2]-bigT^par[2])) - s1
+    t2 <- exp(-(lambdac*bigT)^gammac-par[1]^par[2]*(60^par[2]-bigT^par[2])) - s2
+    terror <- t1^2+t2^2
+    return(terror)
+  }
+  
+  output <- optim(par = c(0, 1), fn = estWeib)
+  lambdat <- output$par[1]
+  gammat <- output$par[2]
+  FlexSimMatrix[i,] <- ifelse(trialtime<=bigT, exp(-(lambdac*trialtime)^gammac), exp(-(lambdac*bigT)^gammac-lambdat^gammat*(trialtime^gammat-bigT^gammat)))
+}
+
+lowerbound <- rep(NA, length(trialtime))
+upperbound <- rep(NA, length(trialtime))
+medianbound <- rep(NA, length(trialtime))
+for (i in 1:length(trialtime)){
+  lowerbound[i] <- quantile(FlexSimMatrix[,i], 0.1, na.rm = T)
+  upperbound[i] <- quantile(FlexSimMatrix[,i], 0.9, na.rm = T)
+  medianbound[i] <- quantile(FlexSimMatrix[,i], 0.5, na.rm = T)
+}
+
+plot(trialtime, controlsurv, type="l", col="blue", xlab = "Time (months)", 
+     yaxt = "n", ylab = "Overall survival (%)", cex.axis=1.5, cex.lab=1.5, cex.main=2)
+axis(2, at=seq(0, 1, by=0.1), labels=seq(0, 100, by=10))
+lines(trialtime, medianbound, col="red")
+lines(trialtime, lowerbound, lty=2)
+lines(trialtime, upperbound, lty=2)
+legend("topright", legend = c("Control", "Treatment", "Treatment CI"), lty = c(1, 1, 2), 
+       col = c("blue", "red", "black"), cex = 1.5)
 dev.off()
 
 
